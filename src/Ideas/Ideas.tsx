c894278f347630b757d80400fea6664e8c976de2
Brian Ogutu <codingbrian58@gmail.com>
2020-04-01 21:54:37 +0300

    Add basic components for ideas

--- src/Ideas/Ideas.tsx
+++ src/Ideas/Ideas.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { Row } from "antd";
+
+import styles from "./Ideas.module.scss";
 
 const Ideas: React.FC = () => {
-  return <div>dsdsds</div>;
+  return <Row className={styles.ideas}></Row>;
 };
 
 export default Ideas;
