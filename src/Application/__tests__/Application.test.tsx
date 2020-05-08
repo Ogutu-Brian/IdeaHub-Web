ee0453406d6514cafa8cf3eba2e4ccc3b0d0573c
Brian Ogutu <codingbrian58@gmail.com>
2020-05-09 00:32:47 +0300

    Fix failing tests

--- src/Application/__tests__/Application.test.tsx
+++ src/Application/__tests__/Application.test.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { mount, ReactWrapper } from "enzyme";
+import React from 'react';
+import { mount, ReactWrapper } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-import Application from "../Application";
+import reducers from '../../store/reducers';
+import Application from '../Application';
 
-describe("Tests for application component", () => {
-  const wrapper: ReactWrapper = mount(<Application />);
+describe('Tests for application component', () => {
+  const store = createStore(reducers, applyMiddleware(thunk));
 
-  it("Should render application component without crushing", () => {
+  const wrapper: ReactWrapper = mount(
+    <Provider store={store}>
+      <Application />
+    </Provider>,
+  );
+
+  it('Should render application component without crushing', () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
