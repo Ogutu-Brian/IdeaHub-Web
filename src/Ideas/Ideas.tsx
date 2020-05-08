d7bf3469badc7b4dbf331c690b6d76150289276d
Brian Ogutu <codingbrian58@gmail.com>
2020-05-09 01:16:24 +0300

    Cleanup code

--- src/Ideas/Ideas.tsx
+++ src/Ideas/Ideas.tsx
@@ -1,22 +1,19 @@
-import React from "react";
-import { Row, Col } from "antd";
+import React from 'react';
+import { Row, Col } from 'antd';
 
 import { Idea } from '../Ideas/components';
 import { Idea as IdeaInterface } from '../interfaces';
 import { MockData } from '../MockData';
 
-import styles from "./Ideas.module.scss";
-
+import styles from './Ideas.module.scss';
 
 const Ideas: React.FC = () => (
-  <Row className={styles.ideas}>{
-    MockData.Ideas.map((idea: IdeaInterface, index: number) =>
+  <Row className={styles.ideas}>
+    {MockData.Ideas.map((idea: IdeaInterface, index: number) => (
       <Col style={{ paddingBottom: '16px' }} key={index}>
-        <Idea
-          idea={idea}
-        />
+        <Idea idea={idea} />
       </Col>
-    )}
+    ))}
   </Row>
 );
 
