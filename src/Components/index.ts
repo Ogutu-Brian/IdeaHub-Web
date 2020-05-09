e82c63fe7989a107eae1fe089e6c7c2c5572cdd1
Brian Ogutu <codingbrian58@gmail.com>
2020-05-09 15:04:49 +0300

    Reorganize the structure of the Search

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,6 +2,5 @@ import { Header, PageHeader } from './Header';
 import { Button } from './Button';
 import { Input, PasswordInput } from './Input';
 import { Icon } from './Icon';
-import { Search } from './Search';
 
-export { Header, Button, Input, PasswordInput, PageHeader, Icon, Search };
+export { Header, Button, Input, PasswordInput, PageHeader, Icon };
