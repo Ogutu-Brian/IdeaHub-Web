17875bf9ae249e9aba9e0e58b3e2ca92dd514b02
Brian Ogutu <codingbrian58@gmail.com>
2020-05-13 01:27:44 +0300

    Fix failing tests

--- src/Components/DropDown/__tests__/Overlay.test.tsx
+++ src/Components/DropDown/__tests__/Overlay.test.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { mount, ReactWrapper } from 'enzyme';
 
 import Overlay from '../Overlay';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Tests for overlay', () => {
-  const wrapper: ReactWrapper = mount(<Overlay />);
+  const wrapper: ReactWrapper = mount(
+    <BrowserRouter>
+      <Overlay />
+    </BrowserRouter>,
+  );
 
   it('Should render overlay without crushing', () => {
     expect(wrapper).toMatchSnapshot();
