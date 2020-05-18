cf0732c91e0a86b2c6ed221e0647b8a1865d8cf7
Brian Ogutu <codingbrian58@gmail.com>
2020-05-18 23:50:11 +0300

    remove build script and add deployment script

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo 'Building IdeaHub...'
-rm -rf build
-yarn build
-cp deploy.sh ./build
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+echo 'Building IdeaHub...'
+rm -rf build
+yarn build
+cp deploy.sh ./build
+cd ./build
 echo 'Deploying IdeaHub...'
 cp -r * /var/www/ideahubweb
 sudo service nginx restart
