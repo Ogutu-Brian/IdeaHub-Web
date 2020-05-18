688ab5b29c0b504ea0ef21d2b8e7042d74844c16
Brian Ogutu <codingbrian58@gmail.com>
2020-05-18 16:14:39 +0300

    Add Profile component

---
+++ src/Profile/__tests__/Profile.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { mount, ReactWrapper } from 'enzyme';
+import Profile from '../Profile';
+
+describe('Tests for profile', () => {
+  const wrapper: ReactWrapper = mount(<Profile />);
+
+  it('Should render profile component without crushing', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
