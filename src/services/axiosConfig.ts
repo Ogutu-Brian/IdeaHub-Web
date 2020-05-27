22956bb58a743f9f4ec3e26c62d2b23f82845410
Brian Ogutu <codingbrian58@gmail.com>
2020-05-27 08:44:26 +0300

    Revert to development environment

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
-const baseURL: string = 'http://159.89.233.98:8082/';
-// const baseURL: string = 'http://127.0.0.1:8000/';
+// const baseURL: string = 'http://159.89.233.98:8082/';
+const baseURL: string = 'http://127.0.0.1:8000/';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL,
