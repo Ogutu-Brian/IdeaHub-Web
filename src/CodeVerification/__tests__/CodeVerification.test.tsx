10a7034734f4090b5f0528c98298522bae3627a2
Brian Ogutu <codingbrian58@gmail.com>
2020-06-01 09:36:52 +0300

    Fix failing tests

--- src/CodeVerification/__tests__/CodeVerification.test.tsx
+++ src/CodeVerification/__tests__/CodeVerification.test.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { mount, ReactWrapper } from 'enzyme';
-
+import { Provider } from 'react-redux';
+import createMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import CodeVerification from '../CodeVerification';
 
 describe('Tests for code verification page', () => {
-  const wrapper: ReactWrapper = mount(<CodeVerification />);
+  const store = createMockStore([thunk])({});
+  const wrapper: ReactWrapper = mount(
+    <Provider store={store}>
+      <CodeVerification />
+    </Provider>,
+  );
 
   it('should render component without crushing', () => {
     expect(wrapper).toMatchSnapshot();
